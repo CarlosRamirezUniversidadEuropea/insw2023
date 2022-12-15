cedb77d821ad3b34352e4765c862db69ff0100cc
carlos.ramirez.lizan <carlos.ramirez.lizan@accenture.com>
2022-12-15 20:39:12 +0100

    json exercise

---
+++ src/main/java/com/ue/insw/proyecto/exercises/json/Main.java
@@ -0,0 +1,41 @@
+package com.ue.insw.proyecto.exercises.json;
+
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        String ruta = "src/main/java/com/ue/insw/proyecto/exercises/json/names.json";
+        // Create a File object for the JSON file
+        File file = new File(ruta);
+        Gson gson = new Gson();
+
+        // Check if the file exists and if we have permission to read it
+        if (file.exists() && file.canRead()) {
+            try {
+                // Read the JSON file into a string
+                Reader reader = Files.newBufferedReader(Paths.get(ruta));
+
+                // Convert the JSON string to a Java object
+                Persona obj = gson.fromJson(reader, Persona.class);
+
+                // Print the data from the Java object
+                System.out.println(obj.getFirst_name());
+                System.out.println(obj.getLast_name());
+                System.out.println(obj.getLocation());
+                System.out.println(obj.getFollowers());
+            } catch (IOException e) {
+                // Handle the IOException
+            }
+        } else {
+            System.out.println("Error: Este programa no ha hecho nada");
+            // Handle the case where the file does not exist or cannot be read
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.ue.insw.proyecto {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires com.google.gson;
+    opens com.ue.insw.proyecto.exercises.json to com.google.gson;
+
     exports com.ue.insw.proyecto.controller;
     opens com.ue.insw.proyecto.controller to javafx.fxml;
     exports com.ue.insw.proyecto.exercises.ej4;
