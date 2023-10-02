7ea3e4d1fd26119daaab9246fed5c308afbf01f7
Jose Vicente Delgado <josevicentedelgado2003@gmail.com>
2023-10-02 20:38:32 +0200

    Ejercicio 0

--- src/main/java/com/ue/insw/proyecto/exercises/ej0documentation/Alumno.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej0documentation/Alumno.java
@@ -1,5 +1,10 @@
 package com.ue.insw.proyecto.exercises.ej0documentation;
-
+/**
+ * Esta clase contiene los atributos y metodos de una Persona
+ * @author Jose Vicente Delgado Berenguer
+ * @version 1.0
+ * @see Alumno
+ */
 import java.util.List;
 
 public class Alumno extends Persona {
@@ -8,7 +13,15 @@ public class Alumno extends Persona {
 	private List<String> asignaturas;
 	private int curso;
 
-
+	/**
+	 *
+	 * @param nombre nombre del alumno
+	 * @param edad edad del alumno
+	 * @param sexo sexo del aluno
+	 * @param numeroExpediente numeroExpediente del alumno
+	 * @param asignaturas asignaturas del alumno
+	 * @param curso curso del alumno
+	 */
 	public Alumno(String nombre, 
 			int edad, 
 			char sexo, 
@@ -21,34 +34,55 @@ public class Alumno extends Persona {
 		this.curso = curso;
 	}
 
-
+	/**
+	 * Metodo para regresar el numero de expediente del alumno
+	 * @return Regresa el numero de expediente del alumno
+	 */
 	public int getNumeroExpediente() {
 		return numeroExpediente;
 	}
 
-
+	/**
+	 * Metodo que le asigna el numero de Expediente del alumno
+	 * @param numeroExpediente, el numero de expediene deseada a asignar
+	 */
 	public void setNumeroExpediente(int numeroExpediente) {
 		this.numeroExpediente = numeroExpediente;
 	}
-
+	/**
+	 * Metodo para regresar las asignaturas del alumno
+	 * @return Regresa las asignaturas del alumno
+	 */
 	public List<String> getAsignaturas() {
 		return asignaturas;
 	}
-
+	/**
+	 * Metodo que le asigna las asignaturas al alumno
+	 * @param asignaturas, asignturas deseadas a asignar
+	 */
 	public void setAsignaturas(List<String> asignaturas) {
 		this.asignaturas = asignaturas;
 	}
 
-
+	/**
+	 * Metodo para regresar el curso del alumno
+	 * @return Regresa el curso del alumno
+	 */
 	public int getCurso() {
 		return curso;
 	}
 
-
+	/**
+	 * Metodo que le asigna el curso del alumno
+	 * @param curso, el curso deseado a asignar
+	 */
 	public void setCurso(int curso) {
 		this.curso = curso;
 	}
-	
+	/**
+	 * Sobrescribe el método toString para obtener una representación de string del objeto Alumno.
+	 *
+	 */
 	@Override
 	public String toString() {
 		return "Alumno [numeroExpediente=" + numeroExpediente + 
--- src/main/java/com/ue/insw/proyecto/exercises/ej0documentation/Main.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej0documentation/Main.java
@@ -1,8 +1,28 @@
 package com.ue.insw.proyecto.exercises.ej0documentation;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        // Creamos una lista de asignaturas utilizando la interfaz List y la clase ArrayList.
+        List<String> asignaturas = new ArrayList<>();
+        // Agregamos dos asignaturas a la lista.
+        asignaturas.add("PCD");
+        asignaturas.add("SOFTWARE");
 
+        // Creamos instancias de diferentes clases utilizando los constructores.
+        // 1. Creamos una instancia de Persona con nombre "Jose", edad 19 y sexo masculino ('H').
+        Persona pepe = new Persona("Jose", 19, 'H');
+
+        // 2. Creamos una instancia de Empleado con nombre "Diegoti", edad 19, sexo masculino ('H'),
+        // número de empleado 1, departamento "Vigilancia espía" y puesto "CEO".
+        Empleado diego = new Empleado("Diegoti", 19, 'H', 1, "Vigilancia espía", "CEO");
+
+        // 3. Creamos una instancia de Alumno con nombre "Paula", edad 20, sexo femenino ('F'),
+        // número de expediente 1234567, lista de asignaturas (asignaturas) y curso 2.
+        Alumno paula = new Alumno("Paula", 20, 'F', 1234567, asignaturas, 2);
     }
 }
