354dbb43d6f06a20a19abad9dde3316f39f0e840
DiegoMoreno <diego.morenogrande@gmail.com>
2023-10-02 20:58:24 +0200

    ej1

--- src/main/java/com/ue/insw/proyecto/exercises/ej0documentation/Alumno.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej0documentation/Alumno.java
@@ -3,12 +3,19 @@ package com.ue.insw.proyecto.exercises.ej0documentation;
 import java.util.List;
 
 public class Alumno extends Persona {
-	
+	/**
+	 * Metodo constructor parametrizado
+	 * @param numeroExpediente Nombre del alumno
+	 * @param asignaturas Edad del alumno
+	 * @param curso Sexo del alumno en formato H o M
+	 */
 	private int numeroExpediente; 
 	private List<String> asignaturas;
 	private int curso;
 
-
+	/**
+	 * Metodo constructor por defecto
+	 */
 	public Alumno(String nombre, 
 			int edad, 
 			char sexo, 
@@ -22,29 +29,50 @@ public class Alumno extends Persona {
 	}
 
 
+	/**
+	 * Metodo para regresar la edad de la persona
+	 * @return Regresa la numeroExpediente de la persona
+	 */
 	public int getNumeroExpediente() {
 		return numeroExpediente;
 	}
 
-
+	/**
+	 * Metodo que le asigna la edad a una persona
+	 * @param numeroExpediente, la edad deseada a asignar
+	 */
 	public void setNumeroExpediente(int numeroExpediente) {
 		this.numeroExpediente = numeroExpediente;
 	}
 
+	/**
+	 * Metodo para regresar el nombre de la persona
+	 * @return Regresa el asignaturas de la persona
+	 */
 	public List<String> getAsignaturas() {
 		return asignaturas;
 	}
 
+	/**
+	 * Metodo que le asigna el nombre a una persona
+	 * @param asignaturas, el nombre deseada a asignar
+	 */
 	public void setAsignaturas(List<String> asignaturas) {
 		this.asignaturas = asignaturas;
 	}
 
-
+	/**
+	 * Metodo para regresar la edad de la persona
+	 * @return Regresa el curso de la persona
+	 */
 	public int getCurso() {
 		return curso;
 	}
 
-
+	/**
+	 * Metodo que le asigna la edad a una persona
+	 * @param curso, la edad deseada a asignar
+	 */
 	public void setCurso(int curso) {
 		this.curso = curso;
 	}
--- src/main/java/com/ue/insw/proyecto/exercises/ej0documentation/Main.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej0documentation/Main.java
@@ -1,8 +1,26 @@
 package com.ue.insw.proyecto.exercises.ej0documentation;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        List<String> asignaturas = new ArrayList<>();
+        asignaturas.add("PCD");
+        asignaturas.add("PEL");
 
+        //Creamos las instancias de diferentes clases utilizando los constructores
+        //1. creamos una instancia de persona con nombre, edad, y sexo
+        Persona nacho = new Persona("antonio", 10, 'M');
+
+        //2. creamos una instancia de empleado con nombre, edad, sexo, numero de empleado, departamento y puesto
+        Empleado josevi = new Empleado("josevi", 13, 'H', 0, "recursos", "patron");
+
+        //3.creamos una instancia de alumno con nombre, edad, sexo, numero de expediente y curso
+        Alumno dieguich = new Alumno( "dieguich", 20, 'M', 888, asignaturas, 7);
+
+        System.out.println(nacho.toString());
+        System.out.println(josevi.toString());
+        System.out.println(dieguich.toString());
     }
 }
