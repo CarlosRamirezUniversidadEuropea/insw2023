d56afb4b31d3d77a6c6a37fd31f6996cf34ac809
Cristina-Guerrero-Cano <cristig.c.10@gmail.com>
2023-10-03 23:29:46 +0200

    Main class completed

--- src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Main.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Main.java
@@ -3,7 +3,16 @@ package com.ue.insw.proyecto.exercises.ej3polimorfismo;
 public class Main {
 
     public static void main(String[] args) {
-        //TODO crear circulo y rectangulo y calcular su area y perímetro
+        
+        // Create a circle and a rectangle and print their perimeter and area
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println(circle);
+        System.out.println("Perimeter: " + circle.calculatePerimeter());
+        System.out.println("Area: " + circle.calculateArea());
+        System.out.println(rectangle);
+        System.out.println("Perimeter: " + rectangle.calculatePerimeter());
+        System.out.println("Area: " + rectangle.calculateArea());
 
     }
 }
