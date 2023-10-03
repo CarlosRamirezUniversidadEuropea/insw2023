5d8207170b94baeb7fd78450779e6e3a7ba2ac45
Cristina-Guerrero-Cano <cristig.c.10@gmail.com>
2023-10-03 23:29:27 +0200

    Shape class completed

--- src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Shape.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Shape.java
@@ -1,8 +1,16 @@
 package com.ue.insw.proyecto.exercises.ej3polimorfismo;
 
+/**
+ * Class Shape is an abstract class that represents a shape: circle, rectangle, etc.
+ * It has a color and methods to calculate the perimeter and area of the shape.
+ */
 public abstract class Shape {
 
+    /**
+     * The color of the shape
+     */
     private String color;
+    
     public abstract double calculatePerimeter();
     public abstract double calculateArea();
     public abstract String toString();
