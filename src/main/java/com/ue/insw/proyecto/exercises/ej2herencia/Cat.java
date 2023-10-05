9ff7705023178d52eed8d1d8199b8dfadfbf8b6e
Jose Vicente Delgado <josevicentedelgado2003@gmail.com>
2023-10-05 20:09:26 +0200

    Ejercicio 2

--- src/main/java/com/ue/insw/proyecto/exercises/ej2herencia/Cat.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej2herencia/Cat.java
@@ -1,5 +1,15 @@
 package com.ue.insw.proyecto.exercises.ej2herencia;
 
-public class Cat {
+public class Cat extends Animal implements Speak{
+    public Cat(String raza, String nombre, int edad) {
+        super(raza, nombre, edad);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Meow");
+    }
+
+
     // todo extends Animal implements speak, override methods
 }
--- src/main/java/com/ue/insw/proyecto/exercises/ej2herencia/Main.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej2herencia/Main.java
@@ -6,5 +6,13 @@ public class Main {
     public static void main(String[] args) {
         //todo crear dog, cat y human y llamar a método speak
 
+        Cat gato = new Cat("Gato","Juan",4);
+        Dog perro = new Dog("Perro","Patas",11);
+        Human humano = new Human("Humano","Lucas",19);
+
+        gato.speak();
+        perro.speak();
+        humano.speak();
+
     }
 }
