aacbefee6e391e114eb5d6b049be128862396629
Cristina-Guerrero-Cano <cristig.c.10@gmail.com>
2023-10-05 21:05:21 +0200

    Adding documentation to Vehicle class

--- src/main/java/com/ue/insw/proyecto/exercises/ej1interfaz/objects/Vehicle.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej1interfaz/objects/Vehicle.java
@@ -4,13 +4,27 @@ import java.math.BigDecimal;
 
 public class Vehicle {
 
+    /**
+     * Price of the vehicle
+     */
     private BigDecimal price;
+    /**
+     * Maximum speed of the vehicle
+     */
     private int speed;
 
+    /**
+     * Constructor of the class Vehicle
+     * @param price
+     */
     public Vehicle(BigDecimal price) {
         this.price = price;
     }
 
+    /**
+     *
+     * @return
+     */
     public BigDecimal getPrice(){
         return price;
     }
