2ab6030b8f7d5484128f0793d5e83f4712122b44
IsmaelDot <Ismael_Dot@outlook.es>
2023-10-05 17:10:18 +0200

    Ejercicio 0: Finalizado main

--- src/main/java/com/ue/insw/proyecto/exercises/ej0documentation/Main.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej0documentation/Main.java
@@ -3,6 +3,12 @@ package com.ue.insw.proyecto.exercises.ej0documentation;
 public class Main {
 
     public static void main(String[] args) {
+        Persona persona = new Persona("Carlos", 20, 'H');
+        Alumno alumno = new Alumno("Juan", 20, 'H', 123456, null, 1);
+        Empleado empleado = new Empleado("Pedro", 20, 'H', 123456, "Sistemas", "Programador");
 
+        System.out.println(persona.getNombre());
+        System.out.println(alumno.getNombre());
+        System.out.println(empleado.getNombre());
     }
 }
