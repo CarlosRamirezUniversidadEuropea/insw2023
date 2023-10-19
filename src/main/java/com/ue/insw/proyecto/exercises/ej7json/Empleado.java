9d6a57651c02b2798bffbefb3c49ec60f309b8f1
paula-saenz <paula.saenz@hotmail.es>
2023-10-19 20:56:38 +0200

    He creado una nueva clase de Empleado, y he leido el json en el main

---
+++ src/main/java/com/ue/insw/proyecto/exercises/ej7json/Empleado.java
@@ -0,0 +1,54 @@
+package com.ue.insw.proyecto.exercises.ej7json;
+
+public class Empleado {
+
+    protected String name;
+    protected String email;
+    protected int phone;
+    protected boolean subscribed;
+    protected String birth;
+
+    public Empleado(String name, String email, int phone, boolean subscribed, String birth) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.subscribed = subscribed;
+        this.birth = birth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail(){ return email; }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() { return phone;}
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public boolean isSubscribed() {
+        return subscribed;
+    }
+
+    public void setSubscribed(boolean subscribed) {
+        this.subscribed = subscribed;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+}
--- src/main/java/com/ue/insw/proyecto/exercises/ej7json/Main.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej7json/Main.java
@@ -11,12 +11,15 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         String ruta = "src/main/java/com/ue/insw/proyecto/exercises/ej7json/person.json";
+        String ruta2 = "src/main/java/com/ue/insw/proyecto/exercises/ej7json/employees.json";
         // Create a File object for the JSON file
         File file = new File(ruta);
         Gson gson = new Gson();
 
+        File file2 = new File(ruta2);
+
         // Check if the file exists and if we have permission to read it
-        if (file.exists() && file.canRead()) {
+        if (file2.exists() && file.canRead()) {
             try {
                 // Read the JSON file into a string
                 Reader reader = Files.newBufferedReader(Paths.get(ruta));
@@ -29,6 +32,31 @@ public class Main {
                 System.out.println(obj.getLast_name());
                 System.out.println(obj.getLocation());
                 System.out.println(obj.getFollowers());
+            } catch (IOException e) {
+                // Handle the IOException
+            }
+        } else {
+            System.out.println("Error: Este programa no ha hecho nada");
+            // Handle the case where the file does not exist or cannot be read
+        }
+
+        if (file2.exists() && file2.canRead()) {
+            try {
+                // Read the JSON file into a string
+                Reader reader2 = Files.newBufferedReader(Paths.get(ruta2));
+
+                // Convert the JSON string to a Java object
+                Empleado[] obj1 = gson.fromJson(reader2, Empleado[].class);
+
+                // Print the data from the Java object
+                for (Empleado empleado : obj1) {
+                    System.out.println(empleado.getName());
+                    System.out.println(empleado.getEmail());
+                    System.out.println(empleado.getPhone());
+                    System.out.println(empleado.isSubscribed());
+                    System.out.println(empleado.getBirth());
+                }
+
             } catch (IOException e) {
                 // Handle the IOException
             }
