02320ebc3d999bb41409e46aba8b31ba1a40c2c4
Cristina-Guerrero-Cano <cristig.c.10@gmail.com>
2023-10-19 22:20:44 +0200

    Finishing object1 class

--- src/main/java/com/ue/insw/proyecto/exercises/ej7json/Object1.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej7json/Object1.java
@@ -1,8 +1,45 @@
 package com.ue.insw.proyecto.exercises.ej7json;
 
+import java.util.List;
+
 public class Object1 {
 
     protected int type;
     protected int num;
     protected List<Value> values;
+
+    @Override
+    public String toString() {
+        return "Object1{" + "type=" + type + ", num=" + num + ", values=" + values + '}';
+    }
+
+    public Object1(int type, int num, List<Value> values) {
+        this.type = type;
+        this.num = num;
+        this.values = values;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public List<Value> getValues() {
+        return values;
+    }
+
+    public void setValues(List<Value> values) {
+        this.values = values;
+    }
 }
