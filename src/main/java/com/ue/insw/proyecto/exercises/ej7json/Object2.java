65399f5960df3f39830b55fe5cdc586464b6f216
Cristina-Guerrero-Cano <cristig.c.10@gmail.com>
2023-10-19 22:20:13 +0200

    Creating Object 2 class

---
+++ src/main/java/com/ue/insw/proyecto/exercises/ej7json/Object2.java
@@ -0,0 +1,54 @@
+package com.ue.insw.proyecto.exercises.ej7json;
+
+import java.util.List;
+
+public class Object2 {
+    protected int type;
+    protected int num;
+    protected List<Fields> fields;
+    protected List<Properties> properties;
+
+    public Object2(int type, int num, List<Fields> fields, List<Properties> properties) {
+        this.type = type;
+        this.num = num;
+        this.fields = fields;
+        this.properties = properties;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public List<Fields> getFields() {
+        return fields;
+    }
+
+    public void setFields(List<Fields> fields) {
+        this.fields = fields;
+    }
+
+    public List<Properties> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(List<Properties> properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public String toString() {
+        return "Object2{" + "type=" + type + ", num=" + num + ", fields=" + fields + ", properties=" + properties + '}';
+    }
+}
