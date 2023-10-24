42cca6f7695f910a4d3cc3abdb8d7286f0e5847a
Luis Lojo <luis.lojo.hervas@gmail.com>
2023-10-24 20:02:20 +0200

    Ejercicio0.hecho Ejercicio3.hecho

--- src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Circle.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Circle.java
@@ -2,4 +2,23 @@ package com.ue.insw.proyecto.exercises.ej3polimorfismo;
 
 //todo extends shape
 public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle with radius " + radius;
+    }
+
 }
