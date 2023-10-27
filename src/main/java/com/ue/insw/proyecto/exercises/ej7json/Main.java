956212dd181cf4d556b596b61d0c7cc411eb115f
Maylen Paredes <maylen020704@gmail.com>
2023-10-27 03:41:54 +0200

    Ejercicio json terminado

--- src/main/java/com/ue/insw/proyecto/exercises/ej7json/Main.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej7json/Main.java
@@ -1,16 +1,19 @@
 package com.ue.insw.proyecto.exercises.ej7json;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         String ruta = "src/main/java/com/ue/insw/proyecto/exercises/ej7json/person.json";
+        String ruta2 = "src/main/java/com/ue/insw/proyecto/exercises/ej7json/employees.json";
         // Create a File object for the JSON file
         File file = new File(ruta);
         Gson gson = new Gson();
@@ -29,7 +32,33 @@ public class Main {
                 System.out.println(obj.getLast_name());
                 System.out.println(obj.getLocation());
                 System.out.println(obj.getFollowers());
+                System.out.println();
             } catch (IOException e) {
+                // Handle the IOException
+            }
+        } else {
+            System.out.println("Error: Este programa no ha hecho nada");
+            // Handle the case where the file does not exist or cannot be read
+        }
+        File file2 = new File(ruta2);
+        Gson gson2 = new Gson();
+
+        if (file2.exists() && file2.canRead()) {
+            try {
+                Reader reader2 = Files.newBufferedReader(Paths.get(ruta2));
+
+                List<Employees> employeesList = gson2.fromJson(reader2, new TypeToken<List<Employees>>(){}.getType());
+
+                for (Employees emp : employeesList) {
+                    System.out.println(emp.getName());
+                    System.out.println(emp.getEmail());
+                    System.out.println(emp.getPhone());
+                    System.out.println(emp.getSubscribed());
+                    System.out.println(emp.getBirth());
+                    System.out.println();
+
+                }
+            } catch (IOException e1) {
                 // Handle the IOException
             }
         } else {
@@ -38,3 +67,5 @@ public class Main {
         }
     }
 }
+
+
