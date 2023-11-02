4f7f23358a14370f1badcf07a93ce77cb43f5a6c
Cristina-Guerrero-Cano <cristig.c.10@gmail.com>
2023-11-02 23:21:55 +0100

    Class Directivo created

--- src/main/java/com/ue/insw/proyecto/exercises/ej5diagramaclases/Directivo.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej5diagramaclases/Directivo.java
@@ -1,5 +1,43 @@
 package com.ue.insw.proyecto.exercises.ej5diagramaclases;
 
-public class Directivo {
-    
+public class Directivo extends Empleado{
+    /**
+     * Category of the manager
+     */
+    private String categoria;
+
+    /**
+     * Constructor
+     * @param name Name of the manager
+     * @param age Age of the manager
+     * @param salary Salary of the manager
+     * @param category Category of the manager
+     */
+    public Directivo(String name, int age, int salary, String category) {
+        super(name, age, salary);
+        this.categoria = category;
+    }
+
+    /**
+     * To string method to print the object
+     */
+    public String mostrar() {
+        return "Directivo{" + "name=" + getNombre() + ", age=" + getEdad() + ", salary=" + getSueldo_bruto() + ", category=" + categoria + '}';
+    }
+
+    /**
+     * Getter for the category of the manager
+     * @return Category of the manager
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * Setter for the category of the manager
+     * @param category Category of the manager
+     */
+    public void setCategoria(String category) {
+        this.categoria = category;
+    }
 }
