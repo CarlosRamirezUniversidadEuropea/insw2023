3f7a4967ecce04d97014b871b0b24fd4490b4549
Cristina-Guerrero-Cano <cristig.c.10@gmail.com>
2023-11-02 23:26:05 +0100

    Class Empresa created

--- src/main/java/com/ue/insw/proyecto/exercises/ej5diagramaclases/Empresa.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej5diagramaclases/Empresa.java
@@ -1,5 +1,39 @@
 package com.ue.insw.proyecto.exercises.ej5diagramaclases;
 
 public class Empresa {
-    
+    /**
+     * Name of the company
+     */
+    private String nombre;
+
+    /**
+     * Constructor
+     * @param name Name of the company
+     */
+    public Empresa(String name) {
+        this.nombre = name;
+    }
+
+    /**
+     * To string method to print the object
+     */
+    public String mostrar() {
+        return "Empresa{" + "name=" + nombre + '}';
+    }
+
+    /**
+     * Getter for the name of the company
+     * @return Name of the company
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Setter for the name of the company
+     * @param name Name of the company
+     */
+    public void setNombre(String name) {
+        this.nombre = name;
+    }
 }
