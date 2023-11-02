4c029c17f3e4945019eebc90837455004d11e385
Cristina-Guerrero-Cano <cristig.c.10@gmail.com>
2023-11-02 23:09:15 +0100

    Class Persona completed

---
+++ src/main/java/com/ue/insw/proyecto/exercises/ej5diagramaclases/Persona.java
@@ -0,0 +1,62 @@
+package com.ue.insw.proyecto.exercises.ej5diagramaclases;
+
+public class Persona {
+    /**
+     * Name of the person
+     */
+    private String nombre;
+    /**
+     * Age of the person
+     */
+    private int edad;
+
+    /**
+     * Constructor
+     * @param name Name of the person
+     * @param age Age of the person
+     */
+    public Persona(String name, int age) {
+        this.nombre = name;
+        this.edad = age;
+    }
+
+    /**
+     * To string method to print the object
+     */
+    public String mostrar() {
+        return "Persona{" + "name=" + nombre + ", age=" + edad + '}';
+    }
+
+    /**
+     * Getter for the name of the person
+     * @return Name of the person
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Setter for the name of the person
+     * @param name Name of the person
+     */
+    public void setNombre(String name) {
+        this.nombre = name;
+    }
+
+    /**
+     * Getter for the age of the person
+     * @return Age of the person
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * Setter for the age of the person
+     * @param age Age of the person
+     */
+    public void setEdad(int age) {
+        this.edad = age;
+    }
+
+}
