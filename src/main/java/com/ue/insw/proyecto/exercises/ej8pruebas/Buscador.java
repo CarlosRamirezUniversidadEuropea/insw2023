6a9a58823c13a4ee0b84610b3e77f16cdff03c0e
Cristina-Guerrero-Cano <cristig.c.10@gmail.com>
2023-11-17 00:41:55 +0100

    Modifying Buscador class according to tests

--- src/main/java/com/ue/insw/proyecto/exercises/ej8pruebas/Buscador.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej8pruebas/Buscador.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Buscador {
 
-    public boolean buscarFrase(String frase, List<String> lista) {
+    /*public boolean buscarFrase(String frase, List<String> lista) {
         for(String phrase: lista){
             if(phrase.equals(frase)){
                 return true;
@@ -13,22 +13,72 @@ public class Buscador {
             }
         }
         return false;
+    }*/
+
+    public boolean buscarFrase(String phrase, List<String> list) {
+        boolean found = false;
+        for (String element : list) {
+            if (element.equals(phrase)) {
+                found = true;
+            }
+        }
+        return found;
     }
+
 
     public boolean buscarPalabra(String palabra, List<String> lista){
         return lista.contains(palabra);
     }
 
-    public String devolverPalabra(List<String> lista, int posicion){
+    /*public String devolverPalabra(List<String> lista, int posicion){
         return lista.get(posicion);
+    }*/
+
+    public String devolverPalabra(List<String> list, int position){
+        String word = "";
+        if (list == null) {
+            word = "Lista vacía";
+        } else if (list.isEmpty()) {
+            word = "Lista vacía";
+        } else {
+            if (position > list.size()) {
+                word = "Posición no válida";
+            } else {
+                word = list.get(position);
+            }
+        }
+        return word;
     }
 
-    public String devolverPrimerElemento(List<String> lista){
+    /*public String devolverPrimerElemento(List<String> lista){
         return lista.get(0);
+    }*/
+    public String devolverPrimerElemento(List<String> list){
+        String word = "";
+        if (list == null) {
+            word = "Lista vacía";
+        } else if (list.isEmpty()) {
+            word = "Lista vacía";
+        } else {
+            word = list.get(0);
+        }
+        return word;
     }
 
-    public String devolverUltimoElemento(List<String> lista){
+    /*public String devolverUltimoElemento(List<String> lista){
         return lista.get(lista.size());
+    }*/
+public String devolverUltimoElemento(List<String> list){
+        String word = "";
+        if (list == null) {
+            word = "Lista vacía";
+        } else if (list.isEmpty()) {
+            word = "Lista vacía";
+        } else {
+            word = list.get(list.size() - 1);
+        }
+        return word;
     }
+
 
 }
