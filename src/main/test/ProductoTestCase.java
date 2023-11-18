d25545dced744d7d9e78c50bda339794e4b07613
Cristina-Guerrero-Cano <cristig.c.10@gmail.com>
2023-11-18 13:02:38 +0100

    Testing producto class methods

--- src/main/test/ProductoTestCase.java
+++ src/main/test/ProductoTestCase.java
@@ -1,6 +1,33 @@
 import AlmacenProductos.Producto;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ProductoTestCase {
 
     Producto producto;
+
+    @BeforeEach
+    void setUp() {
+        producto = new Producto("Coca-Cola", 1.5, 2);
+    }
+
+    @Test
+    @DisplayName("Ensuring that it can add a quantity to the product")
+    void testAniadirCantidad() {
+        producto.añadirCantidad(2);
+        assertEquals(4, producto.getCantidad() ,
+                "it can add a quantity to the product");
+    }
+
+    @Test
+    @DisplayName("Ensuring that it can add a quantity to the product although it is a negative quantity")
+    void testAniadirCantidad2() {
+        producto.añadirCantidad(-2);
+        assertEquals(0, producto.getCantidad() ,
+                "it can add a quantity to the product although it is a negative quantity");
+    }
+
 }
