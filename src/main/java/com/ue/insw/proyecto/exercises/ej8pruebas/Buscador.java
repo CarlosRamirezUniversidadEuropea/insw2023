3c028cfd3e847f5cb874a690271d8b5cd3a259f7
Diego Rodríguez Camarmol <rodriguezcamarmoldiego@gmail.com>
2023-11-18 21:05:42 +0100

    Buscador.java y BuscadorTestCase.java terminados. Queda el ejercicio Gestor-Producto restante.

--- src/main/java/com/ue/insw/proyecto/exercises/ej8pruebas/Buscador.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej8pruebas/Buscador.java
@@ -5,30 +5,49 @@ import java.util.List;
 public class Buscador {
 
     public boolean buscarFrase(String frase, List<String> lista) {
-        for(String phrase: lista){
-            if(phrase.equals(frase)){
+
+        for (String s : lista) {
+            if (frase.equals(s)) {
                 return true;
             } else {
                 return false;
             }
         }
         return false;
+
     }
 
     public boolean buscarPalabra(String palabra, List<String> lista){
-        return lista.contains(palabra);
+        if(lista!=null && palabra!= null){
+            return lista.contains(palabra);
+        }else{
+            return false;
+        }
     }
 
     public String devolverPalabra(List<String> lista, int posicion){
-        return lista.get(posicion);
+        if(posicion>lista.size()){
+            return "Index out of bounds exception";
+        }
+        else {
+            return lista.get(posicion);
+        }
     }
 
     public String devolverPrimerElemento(List<String> lista){
-        return lista.get(0);
+        if(lista!=null) {
+            return lista.get(0);
+        }else{
+            return "NullPointer exception";
+        }
     }
 
     public String devolverUltimoElemento(List<String> lista){
-        return lista.get(lista.size());
+        if(lista!=null) {
+            return lista.get(lista.size()-1);
+        }else{
+            return "NullPointer exception";
+        }
     }
 
 }
---
+++ src/main/test/BuscadorTestCase.java
@@ -0,0 +1,110 @@
+import com.ue.insw.proyecto.exercises.ej8pruebas.Buscador;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BuscadorTestCase {
+
+    Buscador buscador = new Buscador();
+
+    @Test
+    @DisplayName("BuscarFrase should work")
+    void testBuscarFrase(){
+        List<String> listFrases = new ArrayList<String>();
+        listFrases.add("hola");
+        listFrases.add("que");
+        listFrases.add("tal");
+        listFrases.add("me");
+        listFrases.add("llamo");
+        listFrases.add("Diego");
+        assertEquals(false,buscador.buscarFrase("me",listFrases),"Frase econtrada");
+    }
+    @Test
+    @DisplayName("BuscarFrase should work")
+    void testBuscarFraseNull(){
+        List<String> listFrases = new ArrayList<String>();
+        listFrases.add("hola");
+        listFrases.add("que");
+        listFrases.add("tal");
+        listFrases.add("me");
+        listFrases.add("llamo");
+        listFrases.add("Diego");
+        assertEquals(false,buscador.buscarFrase(null,listFrases),"Frase econtrada");
+    }
+    @Test
+    @DisplayName("BuscarFrase should work")
+    void testBuscarFraseListaNull(){
+        assertEquals(false,buscador.buscarFrase("me",null),"Frase econtrada");
+    }
+
+    @Test
+    @DisplayName("BuscarPalabra should work")
+    void testBuscarPalabra(){
+        List<String> listPalabras = new ArrayList<String>();
+        listPalabras.add("a");
+        listPalabras.add("b");
+        listPalabras.add("c");
+        listPalabras.add("d");
+        assertEquals(true,buscador.buscarPalabra("b", listPalabras),"Palabra encontrada");
+    }
+    @Test
+    @DisplayName("BuscarPalabra should work")
+    void testBuscarPalabraNull(){
+        List<String> listPalabras = new ArrayList<String>();
+        listPalabras.add("a");
+        listPalabras.add("b");
+        listPalabras.add("c");
+        listPalabras.add("d");
+        assertEquals(false,buscador.buscarPalabra(null, listPalabras),"Palabra encontrada");
+    }
+    @Test
+    @DisplayName("BuscarPalabra should work")
+    void testBuscarPalabraListaNull(){
+        assertEquals(false,buscador.buscarPalabra("b", null),"Palabra encontrada");
+    }
+    @Test
+    @DisplayName("DevolverPalabra should work")
+    void testDevolverPalabra(){
+        List<String> listPalabras = new ArrayList<String>();
+        listPalabras.add("e");
+        listPalabras.add("f");
+        listPalabras.add("g");
+        listPalabras.add("h");
+        assertEquals("e", buscador.devolverPalabra(listPalabras,0));
+    }
+    @Test
+    @DisplayName("DevolverPrimerElemento should work")
+    void testDevolverPrimerElemento(){
+        List<String> listP = new ArrayList<String>();
+        listP.add("i");
+        listP.add("j");
+        listP.add("k");
+        listP.add("l");
+        assertEquals("i", buscador.devolverPrimerElemento(listP));
+    }
+    @Test
+    @DisplayName("DevolverPrimerElemento should work")
+    void testDevolverPrimerElementoNull(){
+        assertEquals("NullPointer exception", buscador.devolverPrimerElemento(null));
+    }
+    @Test
+    @DisplayName("DevolverPrimerElemento should work")
+    void testDevolverUltimoElemento(){
+        List<String> listP = new ArrayList<String>();
+        listP.add("i");
+        listP.add("j");
+        listP.add("k");
+        listP.add("l");
+        assertEquals("l", buscador.devolverUltimoElemento(listP));
+    }
+    @Test
+    @DisplayName("DevolverPrimerElemento should work")
+    void testDevolverUltimoElementoNull(){
+        assertEquals("NullPointer exception", buscador.devolverUltimoElemento(null));
+    }
+}
