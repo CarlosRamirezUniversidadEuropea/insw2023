da8ae4d9f834434498b3ada196dfa3d27ae6b392
Paula Rey <127853420+Paulardr@users.noreply.github.com>
2023-11-20 23:13:58 +0100

    Ejercicios hechos

---
+++ src/main/java/com/ue/insw/proyecto/exercises/ej8pruebas/Buscador.java
@@ -0,0 +1,38 @@
+package com.ue.insw.proyecto.exercises.ej8pruebas;
+
+import java.util.List;
+
+public class Buscador {
+
+    public boolean buscarFrase(String frase, List<String> lista) {
+        for(String phrase: lista){
+            if(phrase.equals(frase)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean buscarPalabra(String palabra, List<String> lista){
+        if(lista!=null) {
+            return lista.contains(palabra);
+        }
+        return false;
+    }
+
+    public String devolverPalabra(List<String> lista, int posicion){
+        return lista.get(posicion);
+    }
+
+    public String devolverPrimerElemento(List<String> lista){
+        if(lista!=null) {
+            return lista.get(0);
+        }
+        return null;
+    }
+
+    public String devolverUltimoElemento(List<String> lista){
+        return lista.get(lista.size()-1);
+    }
+
+}
