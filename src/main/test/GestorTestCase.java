686585bfc69ce1c6d4b175b8df039fb6571c8861
pablo <22377012@live.uem.es>
2023-11-20 20:07:23 +0100

    -

---
+++ src/main/test/GestorTestCase.java
@@ -0,0 +1,14 @@
+import AlmacenProductos.Gestor;
+import com.ue.insw.proyecto.exercises.ej8pruebas.Calculator;
+import org.junit.jupiter.api.BeforeEach;
+
+public class GestorTestCase {
+
+    Gestor gestor;
+
+    @BeforeEach
+    void setUp() {
+        gestor = new Gestor();
+    }
+
+}
