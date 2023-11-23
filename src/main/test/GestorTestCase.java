bb3324b2538015431428eaeb73d54fecf61a571d
Lucia Medrano Sanchez <22221717@live.uem.es>
2023-11-23 23:08:45 +0100

    Ejercicio pruebas buscador, gestor y producto. Junto con el formulario

--- src/main/test/GestorTestCase.java
+++ src/main/test/GestorTestCase.java
@@ -10,27 +10,27 @@ public class GestorTestCase {
     Gestor gestor;
     @Test
     void añadirProductoAlAlmacenCorrectamente() throws Exception {
-        Gestor gestor = new Gestor();
+        gestor = new Gestor();
         Producto producto = new Producto("ordenador", 500, 5);
         assertEquals(true,gestor.añadirProducto(producto));
     }
 
     @Test
     void intentarAñadirProductoNulo() throws Exception {
-        Gestor gestor = new Gestor();
+        gestor = new Gestor();
         assertEquals(false,gestor.añadirProducto(null));
     }
 
     @Test
     void intentarAñadirProductoExcediendoCantidadMaxima() throws Exception {
-        Gestor gestor = new Gestor();
+        gestor = new Gestor();
         Producto producto = new Producto("Teléfono", 500.0, 30001);
         assertEquals(false,gestor.añadirProducto(producto));
     }
 
     @Test
     void intentarAñadirProductoConCantidadMayorA250() throws Exception {
-        Gestor gestor = new Gestor();
+        gestor = new Gestor();
         Producto producto = new Producto("Tablet", 300.0, 300);
         assertEquals(false,gestor.añadirProducto(producto));
     }
@@ -38,10 +38,12 @@ public class GestorTestCase {
     @Test
     void quitarCantidadDeProductoDelAlmacen() throws Exception {
         try{
-            Gestor gestor = new Gestor();
+            gestor = new Gestor();
             Producto producto = new Producto("Camisa", 25.0, 10);
             gestor.añadirProducto(producto);
-            assertTrue(gestor.quitarXProducto(producto, 3));
+            assertEquals(false, gestor.getAlmacen().isEmpty());
+            gestor.quitarXProducto(producto, 3);
+            assertEquals(false, gestor.getAlmacen().isEmpty());
             assertEquals(7, gestor.getAlmacen().get(0).getCantidad());
         } catch (Exception e){
             throw e;
@@ -51,14 +53,14 @@ public class GestorTestCase {
 
     @Test
     void intentarQuitarCantidadDeProductoInexistente() {
-        Gestor gestor = new Gestor();
-        Producto producto = new Producto("Zapatos", 30.0, 15);
+        gestor = new Gestor();
+        Producto producto = null;
         assertEquals(true, gestor.quitarXProducto(producto, 2));
     }
 
     @Test
     void obtenerListaDeProductosEnElAlmacen() throws Exception {
-        Gestor gestor = new Gestor();
+        gestor = new Gestor();
         Producto producto1 = new Producto("Silla", 50.0, 8);
         Producto producto2 = new Producto("Mesa", 120.0, 5);
         gestor.añadirProducto(producto1);
@@ -71,7 +73,7 @@ public class GestorTestCase {
 
     @Test
     void obtenerCantidadTotalEnElAlmacen() throws Exception {
-        Gestor gestor = new Gestor();
+        gestor = new Gestor();
         Producto producto1 = new Producto("Lámpara", 25.0, 12);
         Producto producto2 = new Producto("Espejo", 40.0, 8);
         gestor.añadirProducto(producto1);
