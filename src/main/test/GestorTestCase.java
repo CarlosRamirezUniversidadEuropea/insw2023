4dc4f9e4d4f11f3d90e243f73bbe622a57236233
LuciaDoval <121610017+LuciaDoval@users.noreply.github.com>
2023-11-23 21:00:58 +0100

    Ejercicios de pruebas de caja negra

--- src/main/test/GestorTestCase.java
+++ src/main/test/GestorTestCase.java
@@ -7,21 +7,69 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GestorTestCase {
 
     Gestor gestor;
     Producto producto;
+
     @BeforeEach
     void setUp() {
         gestor = new Gestor();
         producto = new Producto("PatitoDeGoma", 4.99, 20);
     }
-/**
 
     @Test
     @DisplayName("añadirProducto deberia funcionar")
-    void testAñadirProducto(){
-        assert (true, gestor.añadirProducto(producto), "añadirProducto deberia funcionar");
-    }*/
+    void testAñadirProducto() throws Exception {
+        assertTrue(gestor.añadirProducto(producto), "añadirProducto deberia funcionar");
+    }
+
+    @Test
+    @DisplayName("añadirProducto deberia fucnionar")
+    void añadirProductoNull() throws Exception {
+        producto = null;
+        assertFalse(gestor.añadirProducto(producto), "añadirProducto deberia funcionar");
+    }
+
+    @Test
+    @DisplayName("añadirProducto deberia fucnionar")
+    void añadirProductoMuchaCantidad() throws Exception {
+        producto.añadirCantidad(3000);
+        assertFalse(gestor.añadirProducto(producto), "añadirProducto deberia funcionar");
+    }
+
+    @Test
+    @DisplayName("añadirProducto deberia fucnionar")
+    void añadirProductoNull1() throws Exception {
+        producto.añadirCantidad(-20);
+        assertTrue(gestor.añadirProducto(producto), "añadirProducto deberia funcionar");
+    }
+
+    @Test
+    @DisplayName("quitarXProducto deberia fucnionar")
+    void quitarXProducto() throws Exception {
+        assertTrue(gestor.quitarXProducto(producto, 3), "quitarXProducto deberia funcionar");
+    }
+
+    @Test
+    @DisplayName("quitarXProducto deberia fucnionar")
+    void quitarXProducto3() throws Exception {
+        assertFalse(gestor.quitarXProducto(producto, 3000000), "quitarXProducto deberia funcionar");
+    }
+
+    @Test
+    @DisplayName("quitarXProducto deberia fucnionar")
+    void quitarXProducto4() throws Exception {
+        assertFalse(gestor.quitarXProducto(null, 1), "quitarXProducto deberia funcionar");
+    }
+
+    @Test
+    @DisplayName("quitarXProducto deberia fucnionar")
+    void quitarXProducto5() throws Exception {
+        assertFalse(gestor.quitarXProducto(producto, 0), "quitarXProducto deberia funcionar");
+    }
 
 }
---
+++ src/main/test/ProductoTestCase.java
@@ -0,0 +1,48 @@
+
+import AlmacenProductos.Producto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductoTestCase {
+    Producto producto;
+
+    @BeforeEach
+    void setUp() {
+        producto = new Producto("PatitoDeGoma", 4.99, 20);
+    }
+
+    @Test
+    @DisplayName("getNombre deberia funcionar")
+    void getNombre(){
+        assertEquals("PatitoDeGoma", producto.getNombre(), "getNombre deberia funcionar");
+    }
+
+    @Test
+    @DisplayName("getPrecio deberia funcionar")
+    void getPrecio(){
+        assertEquals(4.99, producto.getPrecio(), "getPrecio deberia funcionar");
+    }
+
+    @Test
+    @DisplayName("getCantidad deberia funcionar")
+    void getCantidad(){
+        assertEquals(20, producto.getCantidad(), "getCantidad deberia funcionar");
+    }
+
+    @Test
+    @DisplayName("getCantidad deberia funcionar")
+    void añadirCantidad(){
+        producto.añadirCantidad(30);
+        assertEquals(50, producto.getCantidad(), "getCantidad deberia funcionar");
+    }
+
+    @Test
+    @DisplayName("getCantidad deberia funcionar")
+    void añadirCantidadResta(){
+        producto.añadirCantidad(-10);
+        assertEquals(10, producto.getCantidad(), "getCantidad deberia funcionar");
+    }
+}
